20760eef2ad2b3b48ded7f20f6ec42deb730c057
ReszkeM <marek.reszke09@gmail.com>
2015-11-28 11:13:48 +0100

    Add user accounts viewModels

---
+++ VolleyballLeagueManagement/VolleyballLeagueManagement.UsersAccounts.Contracts/ViewModels/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using VolleyballLeagueManagement.Common.Enums;
+
+namespace VolleyballLeagueManagement.UsersAccounts.Contracts.ViewModels
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+
+        public int AddressId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordRepeat { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool IsAccountConfirmed { get; set; }
+
+        public Role Role { get; set; }
+
+
+        public AddressViewModel Address { get; set; }
+    }
+}
