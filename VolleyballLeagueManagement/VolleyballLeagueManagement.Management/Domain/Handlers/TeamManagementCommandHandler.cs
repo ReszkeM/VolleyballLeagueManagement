982591759efc1cec85f3fffe06e7ee2aebdb0d72
ReszkeM <marek.reszke09@gmail.com>
2015-12-12 09:15:17 +0100

    Create team command handlers and extensions

---
+++ VolleyballLeagueManagement/VolleyballLeagueManagement.Management/Domain/Handlers/TeamManagementCommandHandler.cs
@@ -0,0 +1,152 @@
+﻿using System.Linq;
+using VolleyballLeagueManagement.Common.Enums;
+using VolleyballLeagueManagement.Common.Infrastructure;
+using VolleyballLeagueManagement.Common.Interfaces;
+using VolleyballLeagueManagement.Management.Domain.Commands;
+using VolleyballLeagueManagement.Management.Model;
+
+namespace VolleyballLeagueManagement.Management.Domain.Handlers
+{
+    public class TeamManagementCommandHandler : 
+        IHandler<AddPlayerCommand>,
+        IHandler<CreateTeamCommand>,
+        IHandler<JoinLeagueCommand>,
+        IHandler<RemovePlayerCommand>,
+        IHandler<RemoveTeamCommand>,
+        IHandler<UpdateCoachCommand>
+    {
+        public void Handle(AddPlayerCommand command)
+        {
+            // TODO validate
+
+            using (var dbContext = new ManagementDataContext())
+            {
+                Team team = dbContext.Teams.SingleOrDefault(l => l.Id == command.TeamId);
+                Player player = CreatePlayerEntity(command, team);
+
+                dbContext.Players.Add(player);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void Handle(CreateTeamCommand command)
+        {
+            // TODO validate
+
+            using (var dbContext = new ManagementDataContext())
+            {
+                Team team = CreateTeamEntity(command);
+
+                dbContext.Teams.Add(team);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void Handle(JoinLeagueCommand command)
+        {
+            // TODO validate
+
+            using (var dbContext = new ManagementDataContext())
+            {
+                League league = dbContext.Leagues.SingleOrDefault(l => l.Id == command.LeagueId);
+                Team team = dbContext.Teams.SingleOrDefault(l => l.Id == command.TeamId);
+
+                JoinToLeague(league, team);
+
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void Handle(RemovePlayerCommand command)
+        {
+            // TODO validate
+
+            using (var dbContext = new ManagementDataContext())
+            {
+                Player player = dbContext.Players.SingleOrDefault(l => l.Id == command.PlayerId);
+
+                dbContext.Players.Remove(player);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void Handle(RemoveTeamCommand command)
+        {
+            // TODO validate
+
+            using (var dbContext = new ManagementDataContext())
+            {
+                Team team = dbContext.Teams.SingleOrDefault(l => l.ManagerId == command.Id);
+
+                dbContext.Teams.Remove(team);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void Handle(UpdateCoachCommand command)
+        {
+            // TODO validate
+
+            using (var dbContext = new ManagementDataContext())
+            {
+                Team team = dbContext.Teams.SingleOrDefault(l => l.Id == command.TeamId);
+
+                if (team == null || team.Coach == null)
+                    throw new ServerSideException("Ups, something went wrong! Refresh page and try agine");
+
+                UpdateCoach(team.Coach, command);
+                dbContext.SaveChanges();
+            }
+        }
+
+
+        private Player CreatePlayerEntity(AddPlayerCommand command, Team team)
+        {
+            return new Player
+            {
+                Team = team,
+                TeamId = command.TeamId,
+                FirstName = command.FirstName,
+                LastName = command.LastName,
+                Age = command.Age,
+                Growth = command.Growth,
+                Position = (Position)command.PositionValue,
+                IsCapitan = command.IsCapitan,
+            };
+        }
+
+        private Team CreateTeamEntity(CreateTeamCommand command)
+        {
+            return new Team
+            {
+                Name = command.Name,
+                ManagerId = command.ManagerId,
+                Status = TeamStatus.LookingForLeague,
+                Coach = new Coach
+                {
+                    FirstName = command.CoachFirstName,
+                    LastName = command.CoachLastName,
+                    Age = command.CoachAge,
+                    Email = command.CoachEmail,
+                    Phone = command.CoachPhone
+                }
+            };
+        }
+
+        private void JoinToLeague(League league, Team team)
+        {
+            league.Teams.Add(team);
+            team.League = league;
+            team.LeagueId = league.Id;
+        }
+
+        private void UpdateCoach(Coach coach, UpdateCoachCommand command)
+        {
+            coach.FirstName = command.CoachFirstName;
+            coach.LastName = command.CoachLastName;
+            coach.Age = command.CoachAge;
+            coach.Email = command.CoachEmail;
+            coach.Phone = command.CoachPhone;
+        }
+    }
+}
---
+++ VolleyballLeagueManagement/VolleyballLeagueManagement.Management/QueryObjects/TeamExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using VolleyballLeagueManagement.Management.Contracts.ViewModels;
+using VolleyballLeagueManagement.Management.Model;
+
+namespace VolleyballLeagueManagement.Management.QueryObjects
+{
+    public static class TeamExtensions
+    {
+        public static TeamPreviewViewModel FindTeamByUserId(this IQueryable<Team> teams, int userId)
+        {
+            var team = teams.FirstOrDefault(u => u.ManagerId == userId);
+
+            if (team == null)
+                return new TeamPreviewViewModel();
+
+            return team.ToViewModel();
+        }
+
+        public static UpdateCoachViewModel GetCoachByUserId(this IQueryable<Team> teams, int userId)
+        {
+            var team = teams.FirstOrDefault(u => u.ManagerId == userId);
+
+            if (team == null || team.Coach == null)
+                throw new ArgumentException(
+                    string.Format("Team with user id: {0} does not exist in the database.", userId));
+
+            return GetCoach(team);
+        }
+
+       
+        public static TeamViewModel CreateNewTeam(int userId)
+        {
+            return new TeamViewModel
+            {
+                ManagerId = userId
+            };
+        }
+
+
+        private static TeamPreviewViewModel ToViewModel(this Team team)
+        {
+            return new TeamPreviewViewModel
+            {
+                Name = team.Name,
+                Status = team.Status,
+                CoachFirstName = team.Coach.FirstName,
+                CoachLastName = team.Coach.LastName
+            };
+        }
+
+        private static UpdateCoachViewModel GetCoach(Team team)
+        {
+            return new UpdateCoachViewModel
+            {
+                TeamId = team.Id,
+                CoachFirstName = team.Coach.FirstName,
+                CoachLastName = team.Coach.LastName,
+                CoachAge = team.Coach.Age,
+                CoachEmail = team.Coach.Email,
+                CoachPhone = team.Coach.Phone,
+            };
+        }
+    }
+}
